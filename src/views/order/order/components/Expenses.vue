833c8b92c7da543f79b3354cc816d27f9ec030d0
Gumtrip <408156312@qq.com>
2021-04-08 16:16:02 +0800

    订单支付

--- src/views/order/order/components/Expenses.vue
+++ src/views/order/order/components/Expenses.vue
@@ -3,22 +3,25 @@
     <el-dialog
       id="userDia"
       :visible.sync="show"
+      title="添加支出"
       width="800px"
       center
       @close="onClose"
     >
-      <el-form ref="form" :model="listQuery" label-width="80px">
-        <el-form-item label="产品列表" style="width: 248px">
-          <el-input v-model="listQuery.filter.title" placeholder="输入产品名字" />
+      <el-form ref="expenseForm" :rules="expenseRules" :model="listQuery" label-width="140px">
+        <el-form-item class="required" label="支出名称:" prop="title">
+          <el-input v-model="postForm.title" />
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary" icon="el-icon-search" @click="getItems" />
+        <el-form-item class="required" label="支出总额(人民币):" prop="total_amount">
+          <el-input v-model="postForm.total_amount" />
+        </el-form-item>
+        <el-form-item label="备注:">
+          <el-input v-model="postForm.remark" type="textarea" rows="2" />
         </el-form-item>
       </el-form>
       <div id="dia-footer">
-        <el-pagination v-show="total>0" :total="total" layout="prev,pager,next" background :page-size="listQuery.size" class="fl" @current-change="changePage" />
         <el-button class="fr" @click="onClose">返回</el-button>
-        <el-button class="fr mr-10" type="primary" @click="chooseItems">确认选择</el-button>
+        <el-button class="fr mr-10" type="primary" @click="submitForm">确认添加</el-button>
         <div class="clearfix" />
       </div>
     </el-dialog>
@@ -26,19 +29,29 @@
 </template>
 
 <script>
-import { fetchProducts } from '@/api/product'
-import { indexOf } from 'lodash'
 
+import { createOrderExpense, updateOrderExpense, fetchOrderExpense } from '@/api/order'
+
+const defaultForm = {
+  title: null,
+  total_amount: 0,
+  remark: '',
+  id: null
+}
 export default {
   name: 'Expenses',
   props: {
     dialog: {
       type: Boolean,
-      default: true
+      default: false
     },
-    items: {
-      type: Array,
-      default: () => function() { return [] }
+    order: {
+      type: Object,
+      default: () => function() { return {} }
+    },
+    id: {
+      type: Number,
+      default: null
     }
   },
   data() {
@@ -52,62 +65,57 @@ export default {
         }
       },
       list: [],
-      multipleSelection: [],
-      initDialog: false
+      postForm: defaultForm,
+      expenseRules: {
+        // title: [{ required: true, message: '支出名称是必填的', trigger: 'blur' }],
+        // total_amount: [{ required: true, message: '支出总额是必填的', trigger: 'blur' }]
+      }
     }
   },
   watch: {
     dialog(newVal, oldVal) {
       this.show = newVal
-      if (!this.initDialog) {
-        // this.init()
+    },
+    id(newVal, oldVal) {
+      if (newVal) {
+        this.getExpense(newVal)
+      } else {
+        this.postForm = defaultForm
       }
     }
   },
   created() {
-    this.getItems()
   },
   methods: {
     onClose() { // 关闭弹窗
       this.$emit('close-dia', false)
     },
-    async getItems() {
-      const res = await fetchProducts(this.listQuery)
-      this.list = res.data.data
-      this.total = res.data.meta.total
-    },
-    async changePage(page) { // 翻页
-      this.listQuery.page = page
-      await this.getItems()
-    },
-    handleSelectionChange(val) { // 勾选多选
-      this.multipleSelection = val
-    },
-    chooseItems() { // 点击选择按钮
-      this.show = false
-      const selections = []
-      const itemIds = []
-      let items = this.items
-
-      this.multipleSelection.forEach(item => {
-        selections.push(item.id)
-      })
-      this.items.forEach(row => {
-        itemIds.push(row.id)
-      })
-      // 没有才增加
-      selections.forEach((id, key) => {
-        if (indexOf(itemIds, id) === -1) {
-          items = items.concat({
-            id: this.multipleSelection[key].id,
-            name: this.multipleSelection[key].title,
-            img: this.multipleSelection[key].main_image,
-            price: 1, // 默认1
-            amount: 1 // 默认是1
-          })
+    async submitForm() {
+      try {
+        // 表单校验不知道为什么不行,难道是一个页面只能一个表单验证？？
+        const valid = await this.$refs.expenseForm.validate()
+        if (valid) {
+          this.postForm.order_id = this.order.id
+          this.loading = true
+          if (this.isEdit) {
+            await updateOrderExpense(this.id, this.postForm)
+          } else {
+            await createOrderExpense(this.postForm)
+          }
+          this.$notify({ title: '成功', message: '操作成功', type: 'success' })
+          this.$emit('update-order', true)
+          this.show = false
+        } else {
+          console.log('error submit!!')
+          this.loading = false
         }
-      })
-      this.$emit('pass-items', items)
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    async getExpense(id) {
+      const res = await fetchOrderExpense(id)
+      this.postForm = res.data
     }
   }
 
@@ -115,11 +123,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  #userDia ::v-deep{
-    .el-dialog__header{padding: 0}
-    .el-dialog__body{padding-top: 0}
-    .el-table__row{cursor: pointer}
-  }
-  #userTable{margin-bottom: 20px}
-  .picBox{width: 80px;height: 80px;margin: 0 auto}
 </style>
--- src/views/order/order/components/OrderDetail.vue
+++ src/views/order/order/components/OrderDetail.vue
@@ -9,7 +9,7 @@
       </sticky>
 
       <div class="createPost-main-container">
-        <el-form-item prop="category_id" label="客户:">
+        <el-form-item prop="category_id" label="客户:" class="required">
           <el-select v-model="postForm.client_id" placeholder="客户" name="category_id" required>
             <el-option v-for="(client,key) in clients" :key="key" :label="client.name" :value="client.id" />
           </el-select>
@@ -23,12 +23,12 @@
             </el-select>
           </el-input>
         </el-form-item>
-        <el-form-item label="汇率:" prop="exchange_rate">
+        <el-form-item label="汇率:" prop="exchange_rate" class="required">
           <el-input v-model="postForm.exchange_rate" class="midFormInput" type="number" placeholder="汇率" />
         </el-form-item>
 
-        <el-form-item label="人民币收入:" prop="exchange_rate">
-          <el-input v-model="postForm.rmb_total_amount" class="midFormInput" type="number" placeholder="人民币收入" />
+        <el-form-item label="人民币收入:">
+          <el-input v-model="postForm.rmb_total_amount" :readonly="true" class="midFormInput" type="number" placeholder="人民币收入" />
         </el-form-item>
         <div id="itemBox">
           <div class="mb-10">
@@ -83,127 +83,85 @@
               label="操作"
             >
               <template slot-scope="scope">
-                <span class="delBtn pointer" @click="delItem(scope.$index)">删除</span>
+                <span class="delBtn pointer red-text" @click="delItem(scope.$index)">删除</span>
               </template>
             </el-table-column>
           </el-table>
         </div>
 
-        <!--收款进度-->
-        <div id="proceedBox">
-          <div class="mb-10">
-            <el-button type="primary" @click="showPop=true">添加收款</el-button>
-          </div>
-          <el-table
-            stripe
-            fit
-            highlight-current-row
-            :data="postForm.order_proceeds"
-            style="width: 100%"
-          >
-            <el-table-column label="序号" width="180">
-              <template slot-scope="scope">
-                <span>{{ scope.row.id }}</span>
-              </template>
-            </el-table-column>
-
-            <el-table-column label="名称" width="180">
-              <template slot-scope="scope">
-                <span>{{ scope.row.name }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column label="币种" width="180">
-              <template slot-scope="scope">
-                <div class="picBox">
-                  <img :src="scope.row.currency" alt="">
-                </div>
-              </template>
-            </el-table-column>
-
-            <el-table-column
-              align="center"
-              label="收款金额"
-            >
-              <template slot-scope="scope">
-                <el-input-number v-model="postForm.order_items[scope.$index].amount" :min="1" />
-              </template>
-            </el-table-column>
-            <el-table-column
-              width="260"
-              align="center"
-              label="汇率"
-            >
-              <template slot-scope="scope">
-                <el-input v-model="postForm.order_items[scope.$index].price" class="text-center" placeholder="实收" />
-              </template>
-            </el-table-column>
-            <el-table-column
-              width="260"
-              align="center"
-              label="人命币金额"
-            >
-              <template slot-scope="scope">
-                <el-input v-model="postForm.order_items[scope.$index].price" class="text-center" placeholder="实收" />
-              </template>
-            </el-table-column>
-            <el-table-column
-              align="center"
-              label="操作"
-            >
-              <template slot-scope="scope">
-                <span class="delBtn pointer" @click="delItem(scope.$index)">删除</span>
-              </template>
-            </el-table-column>
-          </el-table>
-
-        </div>
         <!--订单支出-->
-        <div id="expenseBox">
+        <div v-if="isEdit" id="expenseBox" class="mb-20">
           <div class="mb-10">
-            <el-button type="primary" @click="showPop=true">添加支出</el-button>
+            <el-button type="primary" @click="addExpense">添加支出</el-button>
           </div>
           <el-table
             ref="mulTable"
             stripe
             fit
             highlight-current-row
-            :data="postForm.order_items"
+            :data="postForm.expenses"
             style="width: 100%"
           >
-            <el-table-column label="序号" width="180">
+            <el-table-column label="序号" width="60">
               <template slot-scope="scope">
-                <span>{{ scope.row.id }}</span>
+                <span>{{ scope.$index }}</span>
               </template>
             </el-table-column>
-
-            <el-table-column label="名称" width="180">
-              <template slot-scope="scope">
-                <span>{{ scope.row.name }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column label="图片" width="180">
-              <template slot-scope="scope">
-                <div class="picBox">
-                  <img :src="scope.row.img" alt="">
-                </div>
-              </template>
-            </el-table-column>
-
+            <el-table-column label="名称" width="180" prop="title" />
+            <el-table-column label="总价(CNY)" width="180" prop="total_amount" />
             <el-table-column
               align="center"
-              label="产品数量"
+              label="备注"
+              prop="remark"
+            />
+            <el-table-column
+              align="center"
+              label="操作"
             >
               <template slot-scope="scope">
-                <el-input-number v-model="postForm.order_items[scope.$index].amount" :min="1" />
+                <span class="pointer mr-10" @click="editExpense(scope.row)">编辑</span>
+                <span class="delBtn pointer red-text" @click="delExpense(scope.row)">删除</span>
+              </template>
+            </el-table-column>
+          </el-table>
+
+        </div>
+
+        <!--收款进度-->
+        <div v-if="isEdit" id="proceedBox" class="mb-20">
+          <div class="mb-10">
+            <el-button type="primary" @click="addProceed">添加收款</el-button>
+          </div>
+          <el-table
+            ref="mulTable"
+            stripe
+            fit
+            highlight-current-row
+            :data="postForm.proceeds"
+            style="width: 100%"
+          >
+            <el-table-column label="序号" width="60">
+              <template slot-scope="scope">
+                <span>{{ scope.$index }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="币种" width="180">
+              <template slot-scope="scope">
+                <el-input v-model="postForm.proceeds[scope.$index].currency" />
+              </template>
+            </el-table-column>
+            <el-table-column label="总价(CNY)" width="180">
+              <template slot-scope="scope">
+                <el-input v-model="postForm.proceeds[scope.$index].total_amount" type="number" />
               </template>
             </el-table-column>
             <el-table-column
-              width="260"
               align="center"
-              label="实收"
+              label="汇率"
+              width="100"
             >
               <template slot-scope="scope">
-                <el-input v-model="postForm.order_items[scope.$index].price" class="text-center" placeholder="实收" />
+                <el-input v-model="postForm.proceeds[scope.$index].exchange_rate" />
               </template>
             </el-table-column>
             <el-table-column
@@ -211,29 +169,29 @@
               label="操作"
             >
               <template slot-scope="scope">
-                <span class="delBtn pointer" @click="delItem(scope.$index)">删除</span>
+                <span class="delBtn pointer red-text" @click="delItem(scope.$index)">删除</span>
               </template>
             </el-table-column>
           </el-table>
 
         </div>
+
         <el-form-item label="备注:" prop="remark">
           <el-input v-model="postForm.remark" type="textarea" class="midFormInput" rows="4" placeholder="备注" />
         </el-form-item>
       </div>
     </el-form>
-    <products :dialog="showPop" :items="postForm.order_items" @close-dia="showPop=false" @pass-items="getItems" />
-    <expenses :dialog="expenseBox" :items="postForm.expenses" @close-dia="expenseBox=false" @pass-items="getExpenses" />
-    <proceeds :dialog="proceedBox" :items="postForm.proceeds" @close-dia="proceedBox=false" @pass-items="getProceeds" />
+    <products v-if="postForm.order_items" :dialog="showPop" :items="postForm.order_items" @close-dia="showPop=false" @pass-items="getItems" />
+    <expenses v-if="postForm.expenses" :id="expenseId" :dialog="proceedBox" :order="postForm" @close-dia="proceedBox=false" @update-order="updateOrder" />
+
   </div>
 </template>
 
 <script>
 import Sticky from '@/components/Sticky'
-import Expenses from './Expenses'
-import Proceeds from './Proceeds'
 import Products from './Products'
-import { fetchOrder, createOrder, updateOrder } from '@/api/order'
+import Expenses from './Expenses'
+import { fetchOrder, createOrder, updateOrder, deleteOrderExpense } from '@/api/order'
 import { fetchClients } from '@/api/client'
 import moment from 'moment'
 
@@ -247,7 +205,7 @@ const defaultForm = {
 
 export default {
   name: 'OrderDetail',
-  components: { Sticky, Products, Expenses, Proceeds },
+  components: { Sticky, Products, Expenses },
   props: {
     isEdit: {
       type: Boolean,
@@ -260,12 +218,14 @@ export default {
       postForm: Object.assign({}, defaultForm),
       categories: {},
       clients: {},
+      expenseId: null,
       loading: false,
       updateDate: '',
       showPop: false, // 订单产品弹出层
       proceedBox: false, // 支出收款弹出层
       expenseBox: false, // 支出弹出层
       rules: {
+        exchange_rate: [{ required: true, message: '汇率收入是必填的', trigger: 'blur' }]
       }
     }
   },
@@ -290,15 +250,45 @@ export default {
     getItems(items) {
       this.postForm.order_items = items
     },
-    getExpenses(items) {
-      this.postForm.order_items = items
+    updateOrder() {
+      this.fetchData(this.id)
     },
-    getProceeds(items) {
-      this.postForm.order_items = items
+    addExpense() {
+      this.proceedBox = true
+      this.expenseId = null
+    },
+    editExpense(item) {
+      this.proceedBox = true
+      this.expenseId = item.id
+    },
+    delExpense(item) {
+      this.$confirm('确认删除？').then(() => {
+        deleteOrderExpense(item.id).then(() => {
+          this.$message({
+            message: '删除成功！',
+            type: 'success'
+          })
+          this.fetchData(this.id)
+        })
+      })
+        .catch((e) => {
+          console.log(e)
+        })
+    },
+    addProceed(items) {
+
+    },
+    editProceed() {
+
+    },
+    delProceed() {
+
     },
     async submitForm() {
-      const valid = await this.$refs.postForm.validate()
       try {
+        const valid = await this.$refs.postForm.validate()
+        console.log(this.$refs.postForm)
+        console.log(valid)
         if (valid) {
           this.loading = true
           let res
