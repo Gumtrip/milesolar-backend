703c034c0ec21c58a2b92aef5da6c0f55ebaee4a
Gumtrip <408156312@qq.com>
2021-04-26 16:45:36 +0800

    报价订单 选择产品

--- src/views/order/order/components/Products.vue
+++ src/views/order/order/components/Products.vue
@@ -3,7 +3,6 @@
     <el-dialog
       id="userDia"
       :visible.sync="show"
-      width="800px"
       center
       @close="onClose"
     >
@@ -17,8 +16,8 @@
           </el-form-item>
         </el-form>
       </div>
-      <el-table id="userTable" ref="mulTable" :data="list" stripe fit highlight-current-row style="width: 100%" class="listTable" @selection-change="handleSelectionChange">
-        <el-table-column type="selection" width="50" />
+      <el-table id="userTable" ref="mulTable" :data="list" stripe fit highlight-current-row style="width: 100%" class="listTable">
+        <el-table-column width="50" />
         <el-table-column align="center" label="序号" width="60">
           <template slot-scope="scope">
             <span>{{ scope.row.id }}</span>
@@ -36,11 +35,20 @@
             </div>
           </template>
         </el-table-column>
+        <el-table-column align="center" label="产品属性">
+          <template slot-scope="scope">
+            <span v-show="!scope.row.property_show" @click="getProperty(scope.$index,scope.row.id)">显示属性</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="操作">
+          <template slot-scope="scope">
+            <el-button class="fr mr-10" type="success" @click="chooseItem(scope.row)">确认</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <div id="dia-footer">
         <el-pagination v-show="total>0" :total="total" layout="prev,pager,next" background :page-size="listQuery.size" class="fl" @current-change="changePage" />
         <el-button class="fr" @click="onClose">返回</el-button>
-        <el-button class="fr mr-10" type="primary" @click="chooseItems">确认选择</el-button>
         <div class="clearfix" />
       </div>
     </el-dialog>
@@ -48,8 +56,7 @@
 </template>
 
 <script>
-import { fetchProducts } from '@/api/product'
-import { indexOf } from 'lodash'
+import { fetchProducts, fetchProduct } from '@/api/product'
 
 export default {
   name: 'Product',
@@ -65,7 +72,7 @@ export default {
   },
   data() {
     return {
-      show: false,
+      show: true,
       total: 0,
       listQuery: {
         page: 1,
@@ -74,6 +81,7 @@ export default {
         }
       },
       list: [],
+      propertyCates: [],
       multipleSelection: [],
       initDialog: false
     }
@@ -96,6 +104,9 @@ export default {
     async getItems() {
       const res = await fetchProducts(this.listQuery)
       this.list = res.data.data
+      this.list.forEach((item, key) => {
+        this.list[key].property_show = false
+      })
       this.total = res.data.meta.total
     },
     async changePage(page) { // 翻页
@@ -105,33 +116,18 @@ export default {
     handleSelectionChange(val) { // 勾选多选
       this.multipleSelection = val
     },
-    chooseItems() { // 点击选择按钮
+    chooseItem(item) { // 点击选择按钮
       this.show = false
-      const selections = []
-      const itemIds = []
-      let items = this.items
 
-      this.multipleSelection.forEach(item => {
-        selections.push(item.id)
-      })
-      this.items.forEach(row => {
-        itemIds.push(row.id)
-      })
-      // 没有才增加
-      selections.forEach((id, key) => {
-        if (indexOf(itemIds, id) === -1) {
-          items = items.concat({
-            id: this.multipleSelection[key].id,
-            name: this.multipleSelection[key].title,
-            img: this.multipleSelection[key].main_image,
-            desc: this.multipleSelection[key].offer_desc,
-            price: 1, // 默认1
-            amount: 1 // 默认是1
-          })
-        }
-      })
-      this.$emit('pass-items', items)
+      this.$emit('pass-items', item)
+    },
+    async getProperty(index, id) {
+      this.list[index].property_show = true
+      const res = await fetchProduct(id, { include: 'properties' })
+      console.log(res.data)
+      // 将
     }
+
   }
 
 }
